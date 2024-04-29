3c19e0bc40e49e9f884cad813350370152ef23fb
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-04-28 22:10:54 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DocumentPage from './components/DocumentPage';
+
+function App() {
+  return (
+    <div className="App">
+      <DocumentPage />
+    </div>
+  );
+}
+
+export default App;
